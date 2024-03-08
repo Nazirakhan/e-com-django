b3994341aaa9143b3666f0fe48af49c546384637
Nazirakhan <nazirahammed98@gmail.com>
2024-03-08 20:27:55 +0530

    added message for sucessful registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
+from django.contrib import messages
 # Create your views here.
 
 def register(request):
@@ -16,6 +17,8 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password)
             user.phone = phone
             user.save()
+            messages.success(request,'Registration Successful!')
+            return redirect('register')
     else:
         form = RegistrationForm()
     context = {
