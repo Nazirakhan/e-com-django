a86983e6e9b0bdcdc08de2b2acf9f8239c94e5aa
Nazirakhan <nazirahammed98@gmail.com>
2024-04-02 03:15:21 +0530

    trendy and just add product average ratings added in html

--- ecom/views.py
+++ ecom/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, ReviewRatings
+
 
 # Create your views here.
 def index(request):
     trendy = Product.objects.order_by('-stock').filter(is_available=True)[:4]
     just_add = Product.objects.filter(is_available=True).order_by('-created_date')[:4]
 
+    for trendy_product in trendy:
+        t_reviews = ReviewRatings.objects.filter(product_id=trendy_product.id,status=True)
+    
+    for just_product in just_add:
+        j_reviews = ReviewRatings.objects.filter(product_id=just_product.id,status=True)
+
     context = {
         'trendy' : trendy,
         'just_add':just_add,
+        't_reviews':t_reviews,
+        'j_reviews':j_reviews,
     }
 
     return render(request,'index.html', context)
